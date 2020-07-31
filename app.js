9ace34fd839ee58511f0f3ff6a5f3bc30f6a3bc3
CarlZenggggg <200402235@student.georgianc.on.ca>
2020-08-01 00:40:07 +0800

    A description of the changes I've made

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ app.get('/test', (req, res) => {
   res.status(200).json({message: 'Hello World'});
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
 // Start our server
